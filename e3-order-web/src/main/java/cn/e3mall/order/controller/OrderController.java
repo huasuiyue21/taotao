51e15cc409ad9327b05ef99bc6795b6aee729465
huasuiyue21 <2627750344@qq.com>
2019-03-28 12:56:05 +0800

    e3-order-web 订单管理web层

---
+++ e3-order-web/src/main/java/cn/e3mall/order/controller/OrderController.java
@@ -0,0 +1,63 @@
+package cn.e3mall.order.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import cn.e3mall.cart.service.CartService;
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.order.pojo.OrderInfo;
+import cn.e3mall.order.service.OrderService;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbUser;
+
+/**
+ * 订单关联controller
+ * <p>Title: OrderController</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Controller
+public class OrderController {
+
+	@Autowired
+	private CartService cartService;
+	@Autowired
+	private OrderService orderService;
+	
+	@RequestMapping("/order/order-cart")
+	public String showOrderCart(HttpServletRequest request) {
+		TbUser user = (TbUser) request.getAttribute("user");
+		//根据用户id取购物车列表
+		List<TbItem> cartList = cartService.getCartList(user.getId());
+		request.setAttribute("cartList", cartList);
+		return "order-cart";
+	}
+	
+	@RequestMapping(value="/order/create",method=RequestMethod.POST)
+	public String createOrder(OrderInfo orderInfo,HttpServletRequest request) {
+		//取用户信息
+		TbUser user = (TbUser) request.getAttribute("user");
+		//把用户信息天啊家到orderInfo中
+		orderInfo.setUserId(user.getId());
+		orderInfo.setBuyerNick(user.getUsername());
+		//调用服务生成订单
+		E3Result e3Result = orderService.createOrder(orderInfo);
+		if(e3Result.getStatus()==200) {
+			//清空购物车
+			cartService.clearCarItem(user.getId());
+		}
+		//把单号传递给页面
+		request.setAttribute("orderId", e3Result.getData());
+		request.setAttribute("payment", orderInfo.getPayment());
+		//返回逻辑视图
+		return "success";
+	}
+}
